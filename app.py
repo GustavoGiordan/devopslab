3b73f092739055533185385b0717d23f7d2104e3
Gustavo <gustavo.giordan@gmail.com>
2022-05-28 10:07:52 -0300

    alteração teste

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - alterado"
+    return "Hello World - alterado 2"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
